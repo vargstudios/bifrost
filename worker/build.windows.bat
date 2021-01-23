11d98265486435df2b7d49c3c12b775e22fb4928
Per Thomas Lundal <pt@vargstudios.no>
2021-01-23 17:01:01 +0100

    Add windows build script

---
+++ worker/build.windows.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+gradlew build --info "-Dquarkus.package.type=native" "-Dquarkus.native.additional-build-args=--static" "-Dquarkus.native.graalvm-home=C:\dev\graalvm-ce-java11-21.0.0"
