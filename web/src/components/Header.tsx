7fad7690e764d6b43982eab4f07abcfa2a797d61
Per Thomas Lundal <pt@vargstudios.no>
2021-01-31 13:21:19 +0100

    Optimize bundle size

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCog, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faCog } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 export function Header(): JSX.Element {
