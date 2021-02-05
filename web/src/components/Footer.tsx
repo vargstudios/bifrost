4fb7a87ff229677ded16982b1bfd51b6457b84a3
Per Thomas Lundal <pt@vargstudios.no>
2021-02-05 16:31:39 +0100

    Update footer

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 
 export function Footer(): JSX.Element {
-  return <footer>Made with ♥ by VARG Studios</footer>;
+  return (
+    <footer>
+      Made with <span title="blood, sweat and tears">♥</span> by{" "}
+      <span title="a flatmate">VARG Studios</span>
+    </footer>
+  );
 }
