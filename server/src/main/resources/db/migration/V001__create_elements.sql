548efdde2070f74ea04bb740e533d251920321bf
Per Thomas Lundal <pt@vargstudios.no>
2021-02-13 16:50:15 +0100

    Remove testdata

--- server/src/main/resources/db/migration/V001__create_elements.sql
+++ server/src/main/resources/db/migration/V001__create_elements.sql
@@ -35,19 +35,3 @@ create table element_frames
     number     int  not null,
     transcoded int  not null
 );
-
--- Testdata
-
-insert into element_categories (id, name, created)
-values ('v1f', 'Fire', 0),
-       ('esu', 'Blood', 0),
-       ('xfn', 'Smoke', 0),
-       ('9ej', 'Particles', 0);
-
-insert into elements (id, category_id, name, framecount, framerate, channels, alpha, previews, created)
-values ('bnfu', 'v1f', 'Fire from below', 120, 30, 'RGB', 0, 1, 0),
-       ('3uvm', 'v1f', 'Fire from above', 180, 30, 'RGB', 0, 1, 0),
-       ('vmfa', 'v1f', 'Fire from within', 240, 60, 'RGBA', 1, 1, 0),
-       ('ufra', 'esu', 'Blood splatter 1', 120, 30, 'RGBA', 1, 1, 0),
-       ('u3rn', 'esu', 'Blood splatter 2', 160, 30, 'RGBA', 1, 1, 0),
-       ('qwfv', 'esu', 'Blood splatter 3', 180, 30, 'RGBA', 1, 1, 0);
