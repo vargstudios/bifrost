f91abe05c8abed605d0665ef51bac2d1be9645f4
Per Thomas Lundal <pt@vargstudios.no>
2021-02-18 21:16:47 +0100

    Add logo

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -2,13 +2,17 @@ import * as React from "react";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { IconButtonLink } from "./IconButtonLink";
+// @ts-ignore
+import logo from "../logo.svg";
 
 export function Header(): JSX.Element {
   return (
     <header>
       <Link to="/" className="title">
-        BIFROST
+        <img className="logo" src={logo} alt="" />
+        <span className="text">BIFROST</span>
       </Link>
+      <div className="expand" />
       <IconButtonLink to="/import/element" size="large" icon={faCog} />
     </header>
   );
