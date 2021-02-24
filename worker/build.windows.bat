6b5f2d70f28ad0d080ea5289a12b1cf20ab8bd99
Per Thomas Lundal <pt@vargstudios.no>
2021-02-24 19:36:29 +0100

    Fix windows build script

--- worker/build.windows.bat
+++ worker/build.windows.bat
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-gradlew clean
-gradlew build --info "-Dquarkus.native.container-runtime=" "-Dquarkus.native.graalvm-home=C:\dev\graalvm-ce-java11-21.0.0"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call gradlew clean
+call gradlew build --info "-Dquarkus.native.container-runtime=" "-Dquarkus.native.graalvm-home=C:\dev\graalvm-ce-java11-21.0.0"
